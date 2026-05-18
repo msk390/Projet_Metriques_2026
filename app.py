865070cd36ffd456134a2b2b03581d4ddedf78ce
msk390 <pro.ouazil.mimoun@gmail.com>
2026-05-18 15:22:28 +0200

    Fix typo in comment in app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-# Déposez votre code à partir d'ici :
+# Déposez votre code à partir d'ici o :
 
 @app.route("/contact")
 def MaPremiereAPI():
